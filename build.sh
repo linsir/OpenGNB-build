4df1e0ff579bd8ffb7afe57ed0badff356def77b
Linsir <linsir@live.com>
2024-03-17 16:12:16 +0800

    fix: bad ipq6000

--- build.sh
+++ build.sh
@@ -17,91 +17,35 @@ log_info "GNB_VERSION: ${GNB_VERSION}"
 log_info "GITHUB_WORKSPACE: ${GITHUB_WORKSPACE}"
 
 
+build_openwrt_bin(){
+    arch_version=$1
+    toolchain_version=$2
+    cpu_arch=$3
+    echo_separator
+    log_info "build ${arch_version}"
+    cd $GITHUB_WORKSPACE
+    log_info "download http://oss.apichop.com/gnb/build/openwrt-sdk-${arch_version}-simple.tar.gz"
+    mkdir -p $GITHUB_WORKSPACE/${arch_version}
+    curl -s http://oss.apichop.com/gnb/build/openwrt-sdk-${arch_version}-simple.tar.gz |tar -zx -C $GITHUB_WORKSPACE/${arch_version}
 
-# build 19.07.2-ar71xx
-echo_separator
-log_info "build 19.07.2-ar71xx"
-cd $GITHUB_WORKSPACE
-log_info "download http://oss.apichop.com/gnb/build/openwrt-sdk-19.07.2-ar71xx-simple.tar.gz"
-mkdir -p $GITHUB_WORKSPACE/19.07.2-ar71xx
-curl -s http://oss.apichop.com/gnb/build/openwrt-sdk-19.07.2-ar71xx-simple.tar.gz |tar -zx -C $GITHUB_WORKSPACE/19.07.2-ar71xx
+    export PATH=$PATH:$GITHUB_WORKSPACE/${arch_version}/staging_dir/toolchain-${toolchain_version}/bin/
+    export STAGING_DIR=$GITHUB_WORKSPACE/${arch_version}/staging_dir/
+    export CC=${cpu_arch}-openwrt-linux-gcc
+    export CXX=${cpu_arch}-openwrt-linux-g++
 
-export PATH=$PATH:$GITHUB_WORKSPACE/19.07.2-ar71xx/staging_dir/toolchain-mips_24kc_gcc-7.5.0_musl/bin/
-export STAGING_DIR=$GITHUB_WORKSPACE/19.07.2-ar71xx/staging_dir/
-export CC=mips-openwrt-linux-gcc
-export CXX=mips-openwrt-linux-g++
+    cd opengnb
 
-cd opengnb
-make -f Makefile.openwrt clean &&make -f Makefile.openwrt install
+    make -f Makefile.openwrt clean &&make -f Makefile.openwrt install
+    log_info "build opengnb_${GNB_VERSION}_${arch_version}.tar.gz to $GITHUB_WORKSPACE/dist"
+    tar zcf opengnb_${GNB_VERSION}_${arch_version}.tar.gz -C bin .
+    mkdir -p $GITHUB_WORKSPACE/dist
+    cp -af opengnb_${GNB_VERSION}_${arch_version}.tar.gz $GITHUB_WORKSPACE/dist
+    log_info "build ${arch_version} done"
+    }
 
-log_info "build opengnb_${GNB_VERSION}_19.07.2-ar71xx.tar.gz to $GITHUB_WORKSPACE/dist"
-tar zcf opengnb_${GNB_VERSION}_19.07.2-ar71xx.tar.gz -C bin .
-mkdir -p $GITHUB_WORKSPACE/dist
-cp -af opengnb_${GNB_VERSION}_19.07.2-ar71xx.tar.gz $GITHUB_WORKSPACE/dist
-
-log_info "build 19.07.2-ar71xx done"
-
-
-# build 21.02.0-mt76x8
-echo_separator
-log_info "build 21.02.0-mt76x8"
-cd $GITHUB_WORKSPACE
-
-log_info "download http://oss.apichop.com/gnb/build/openwrt-sdk-21.02.0-mt76x8_simple.tar.gz"
-mkdir -p $GITHUB_WORKSPACE/21.02.0-mt76x8
-curl -s http://oss.apichop.com/gnb/build/openwrt-sdk-21.02.0-mt76x8_simple.tar.gz |tar -zx -C $GITHUB_WORKSPACE/21.02.0-mt76x8
-
-export PATH=$PATH:$GITHUB_WORKSPACE/21.02.0-mt76x8/staging_dir/toolchain-mipsel_24kc_gcc-8.4.0_musl/bin/
-export STAGING_DIR=$GITHUB_WORKSPACE/21.02.0-mt76x8/staging_dir/
-export CC=mipsel-openwrt-linux-gcc
-export CXX=mipsel-openwrt-linux-g++
-
-cd opengnb
-make -f Makefile.openwrt clean &&make -f Makefile.openwrt install
-
-tar zcf opengnb_${GNB_VERSION}_21.02.0-mt76x8.tar.gz -C bin .
-mkdir -p $GITHUB_WORKSPACE/dist
-cp -af opengnb_${GNB_VERSION}_21.02.0-mt76x8.tar.gz $GITHUB_WORKSPACE/dist
-log_info "build 21.02.0-mt76x8 done"
-
-# build 22.7.7-mt7621
-echo_separator
-log_info "build 22.7.7-mt7621"
-cd $GITHUB_WORKSPACE
-log_info "download http://oss.apichop.com/gnb/build/openwrt-sdk-22.7.7-mt7621_simple.tar.gz"
-mkdir -p $GITHUB_WORKSPACE/22.7.7-mt7621
-curl -s http://oss.apichop.com/gnb/build/openwrt-sdk-22.7.7-mt7621_simple.tar.gz |tar -zx -C $GITHUB_WORKSPACE/22.7.7-mt7621
-
-export PATH=$PATH:$GITHUB_WORKSPACE/22.7.7-mt7621/staging_dir/toolchain-mipsel_24kc_gcc-8.4.0_musl/bin/
-export STAGING_DIR=$GITHUB_WORKSPACE/22.7.7-mt7621/staging_dir/
-export CC=mipsel-openwrt-linux-gcc
-export CXX=mipsel-openwrt-linux-g++
-
-cd opengnb
-make -f Makefile.openwrt clean &&make -f Makefile.openwrt install
-
-tar zcf opengnb_${GNB_VERSION}_22.7.7-mt7621.tar.gz -C bin .
-mkdir -p $GITHUB_WORKSPACE/dist
-cp -af opengnb_${GNB_VERSION}_22.7.7-mt7621.tar.gz $GITHUB_WORKSPACE/dist
-log_info "build 22.7.7-mt7621 done"
-
-# build 23.11.20-ipq6000
-echo_separator
-log_info "build 23.11.20-ipq6000"
-cd $GITHUB_WORKSPACE
-log_info "download http://oss.apichop.com/gnb/build/openwrt-sdk-23.11.20-ipq6000_simple.tar.gz"
-mkdir -p $GITHUB_WORKSPACE/23.11.20-ipq6000
-curl -s http://oss.apichop.com/gnb/build/openwrt-sdk-23.11.20-ipq6000_simple.tar.gz |tar -zx -C $GITHUB_WORKSPACE/23.11.20-ipq6000
-
-export PATH=$PATH:$GITHUB_WORKSPACE/23.11.20-ipq6000/staging_dir/toolchain-aarch64_cortex-a53_gcc-7.5.0_musl/bin/
-export STAGING_DIR=$GITHUB_WORKSPACE/23.11.20-ipq6000/staging_dir/
-export CC=mipsel-openwrt-linux-gcc
-export CXX=mipsel-openwrt-linux-g++
-
-cd opengnb
-make -f Makefile.openwrt clean &&make -f Makefile.openwrt install
-
-tar zcf opengnb_${GNB_VERSION}_23.11.20-ipq6000.tar.gz -C bin .
-mkdir -p $GITHUB_WORKSPACE/dist
-cp -af opengnb_${GNB_VERSION}_23.11.20-ipq6000.tar.gz $GITHUB_WORKSPACE/dist
-log_info "build 23.11.20-ipq6000 done"
+main(){
+    build_openwrt_bin "19.07.2-ar71xx" "toolchain-mips_24kc_gcc-7.5.0_musl" "mips"
+    build_openwrt_bin "21.02.0-mt76x8" "mipsel_24kc_gcc-8.4.0_musl" "mipsel"
+    build_openwrt_bin "22.7.7-mt7621" "mipsel_24kc_gcc-8.4.0_musl" "mipsel"
+    build_openwrt_bin "23.11.20-ipq6000" "aarch64_cortex-a53_gcc-7.5.0_musl" "aarch64"
+}
